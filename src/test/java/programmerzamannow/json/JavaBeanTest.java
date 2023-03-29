e42bd89f2778ba30a410331b4dc3509709409be5
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-03-29 12:45:48 +0700

    Konversi JSON ke Object

--- src/test/java/programmerzamannow/json/JavaBeanTest.java
+++ src/test/java/programmerzamannow/json/JavaBeanTest.java
@@ -2,6 +2,7 @@ package programmerzamannow.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -25,5 +26,33 @@ public class JavaBeanTest {
         String json = objectMapper.writeValueAsString(person);
 
         System.out.println(json);
+    }
+
+    @Test
+    void readObjectFromJson() throws JsonProcessingException {
+        String json = """
+                {
+                  "id": "1",
+                  "name": "Eko",
+                  "hobbies": [
+                    "Coding",
+                    "Reading"
+                  ],
+                  "address": {
+                    "street": "Jalan belum jadi",
+                    "city": "Jakarta",
+                    "country": "Indonesia"
+                  }
+                }
+                """;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Person person = objectMapper.readValue(json, Person.class);
+
+        Assertions.assertEquals("1", person.getId());
+        Assertions.assertEquals("Eko", person.getName());
+        Assertions.assertEquals("Jalan belum jadi", person.getAddress().getStreet());
+        Assertions.assertEquals("Jakarta", person.getAddress().getCity());
+        Assertions.assertEquals("Indonesia", person.getAddress().getCountry());
     }
 }
