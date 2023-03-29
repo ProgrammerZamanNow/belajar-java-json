4aa0dacf4d8c6a128c451bd36b0300202738f2a0
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-03-29 12:29:59 +0700

    Membaca JSON Object

--- src/test/java/programmerzamannow/json/JsonObjectTest.java
+++ src/test/java/programmerzamannow/json/JsonObjectTest.java
@@ -1,7 +1,9 @@
 package programmerzamannow.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -26,5 +28,29 @@ public class JsonObjectTest {
         String json = objectMapper.writeValueAsString(person);
 
         System.out.println(json);
+    }
+
+    @Test
+    void readJson() throws JsonProcessingException {
+        String json = """
+                {
+                  "age": 30,
+                  "address": {
+                    "street": "Jalan belum ada",
+                    "city": "Jakarta",
+                    "country": "Indonesia"
+                  },
+                  "firstName": "Budi",
+                  "lastName": "Nugraha",
+                  "married": true
+                }
+                """;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> person = objectMapper.readValue(json, new TypeReference<Map<String, Object>>() {
+        });
+
+        Assertions.assertEquals("Budi", person.get("firstName"));
+        Assertions.assertEquals("Nugraha", person.get("lastName"));
     }
 }
