36b524eaa16dec5a2c07d443a671f6042a6d98d3
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-03-29 20:04:17 +0700

    Jacksonn Annontation

--- src/main/java/programmerzamannow/json/Person.java
+++ src/main/java/programmerzamannow/json/Person.java
@@ -1,5 +1,9 @@
 package programmerzamannow.json;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Date;
 import java.util.List;
 
@@ -9,13 +13,36 @@ public class Person {
 
     private String name;
 
+    @JsonProperty("full_name")
+    private String fullName;
+
+    @JsonIgnore
+    private String password;
+
     private List<String> hobbies;
 
     private Address address;
 
     private Date createdAt;
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date updatedAt;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public Date getCreatedAt() {
         return createdAt;
---
+++ src/test/java/programmerzamannow/json/AnnotationTest.java
@@ -0,0 +1,32 @@
+package programmerzamannow.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AnnotationTest {
+
+    @Test
+    void annotation() throws JsonProcessingException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        ObjectMapper objectMapper = new ObjectMapper()
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .setDateFormat(dateFormat);
+
+        Person person = new Person();
+        person.setId("1");
+        person.setName("Eko");
+        person.setFullName("Eko Kurniawan Khannedy");
+        person.setPassword("rahasia");
+        person.setCreatedAt(new Date());
+        person.setUpdatedAt(new Date());
+
+        String json = objectMapper.writeValueAsString(person);
+        System.out.println(json);
+    }
+}
