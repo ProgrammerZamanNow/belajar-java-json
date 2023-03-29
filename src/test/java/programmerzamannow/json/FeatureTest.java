41627b844bb42fb8f8ea9d0a2675ac6c4cdea6dd
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-03-29 12:58:52 +0700

    Deserialization Feature

--- src/test/java/programmerzamannow/json/FeatureTest.java
+++ src/test/java/programmerzamannow/json/FeatureTest.java
@@ -1,10 +1,13 @@
 package programmerzamannow.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class FeatureTest {
 
@@ -20,5 +23,22 @@ public class FeatureTest {
         Person person = objectMapper.readValue(json, Person.class);
         Assertions.assertEquals("1", person.getId());
         Assertions.assertEquals("Eko", person.getName());
+    }
+
+    @Test
+    void deserializationFeature() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+
+        String json = """
+                {"id" : "1", "name": "Eko", "age": 30, "hobbies" : "Coding"}
+                """;
+
+        Person person = objectMapper.readValue(json, Person.class);
+        Assertions.assertEquals("1", person.getId());
+        Assertions.assertEquals("Eko", person.getName());
+        Assertions.assertEquals(List.of("Coding"), person.getHobbies());
     }
 }
